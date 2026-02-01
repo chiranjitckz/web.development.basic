1f373f9bce73378f5ea53acead61f6139a1b299b
chiranjit c <chiranjitc184@gmail.com>
2026-02-01 14:35:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function showMessage() {
     document.getElementById("message").innerText =
-        "Web Development is an essential skill for building modern, interactive, and responsive websites.";
+        "Web Development is an essential skill that focuses on creating modern, interactive, and responsive websites. " +
+        "It involves using HTML to structure content, CSS to design and style webpages, and JavaScript to add " +
+        "interactivity and dynamic behavior. These technologies work together to improve user experience and " +
+        "enable websites to function efficiently across different devices and screen sizes.";
 }
