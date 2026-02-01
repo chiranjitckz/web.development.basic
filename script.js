24ad03cff8c3267bed8934a06337c00d1749262b
chiranjit c <chiranjitc184@gmail.com>
2026-02-01 14:29:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerText =
+        "Web Development is an essential skill for building modern, interactive, and responsive websites.";
+}
